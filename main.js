46c63400296752a51253ddd3aa97081c68a22f50
HiagoFiochi <hiagofiochiramos@gmail.com>
2023-05-23 20:39:44 -0300

    configura por evento

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
-const form = document.forms.entrada;
-        form.addEventListener('submit', envia);
+window.addEventListener('load', setup);
 
-        function envia(evento){
-            evento.preventDefault();
-            console.log('Formulario enviado!');
-        }
+function setup(){
+    const form = document.forms.entrada;
+    form.addEventListener('submit', envia);
+}
+
+function envia(evento){
+    evento.preventDefault();
+    console.log('Formulario enviado!');
+}
