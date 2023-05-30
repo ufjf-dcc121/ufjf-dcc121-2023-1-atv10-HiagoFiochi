2eacdceae078125d5854515b681777f5fc2078d5
HiagoFiochi <hiagofiochiramos@gmail.com>
2023-05-30 19:56:26 -0300

    adiciona exportação padrão

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
-import { estado } from "./store.js";
+import store from "./store.js";
 
 const form = document.forms.entrada;
 form.addEventListener('submit', envia);
 
+atualiza();
+
 function envia(evento){
     evento.preventDefault();
     console.log('Formulário Enviado!');
-    estado++;
+    store.estado++;
     atualiza();
 }
 
 function atualiza(){
     const ol = document.querySelector('ol');
-    ol.innerHTML = '<li>${estado}</li>';
+    ol.innerHTML = '<li>${store.estado}</li>';
 }
